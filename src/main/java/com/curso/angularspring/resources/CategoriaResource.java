f68e43fa7e4d8806b5568348ba390b0d364af17b
gianhp12 <gian_htc@hotmail.com>
2023-05-28 11:29:52 -0300

    implementacao-get-post-categoria

--- src/main/java/com/curso/angularspring/resources/CategoriaResource.java
+++ src/main/java/com/curso/angularspring/resources/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.curso.angularspring.resources;
 
 import com.curso.angularspring.domain.Categoria;
+import com.curso.angularspring.dto.CategoriaDTO;
 import com.curso.angularspring.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -18,5 +21,17 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
         Categoria obj = service.findByID(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/curso/angularspring/service/CategoriaService.java
+++ src/main/java/com/curso/angularspring/service/CategoriaService.java
@@ -6,6 +6,7 @@ import com.curso.angularspring.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,5 +16,12 @@ public class CategoriaService {
     public Categoria findByID(Integer id){
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
+    }
+    public List<Categoria> findAll(){
+        return repository.findAll();
+    }
+    public Categoria create(Categoria obj){
+        obj.setId(null);
+        return repository.save(obj);
     }
 }
